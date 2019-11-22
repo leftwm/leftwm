abff5515aba1fb4d3b49be88212ee730abf3b9f9
Colin Wallace <colin@mooooo.ooo>
2019-11-21 16:03:32 -0800

    Add a GridHorizontal layout, which splits the workspace into an even NxN grid (or as close as it can get)

--- src/layouts/mod.rs
+++ src/layouts/mod.rs
@@ -25,6 +25,7 @@ impl Clone for Box<Layout> {
 pub fn get_all_layouts() -> VecDeque<Box<Layout>> {
     let mut layouts = VecDeque::new();
     layouts.push_back(Box::new(MainAndVertStack) as Box<Layout>);
+    layouts.push_back(Box::new(GridHorizontal) as Box<Layout>);
     layouts.push_back(Box::new(EvenHorizontal) as Box<Layout>);
     layouts.push_back(Box::new(EvenVertical) as Box<Layout>);
     layouts
@@ -103,6 +104,54 @@ impl Layout for MainAndVertStack {
             w.set_x(workspace.x() + width);
             w.set_y(workspace.y() + y);
             y += height;
+        }
+    }
+}
+
+/// Layout which splits the workspace into N columns, and then splits each column into rows.
+/// Example arrangement (4 windows):
+/// ```
+/// +---+---+
+/// |   |   |
+/// +---+---+
+/// |   |   |
+/// +---+---+
+/// ```
+/// or with 8 windows:
+/// ```
+/// +---+---+---+
+/// |   |   |   |
+/// |   +---+---+
+/// +---+   |   |
+/// |   +---+---+
+/// |   |   |   |
+/// +---+---+---+
+/// ```
+#[derive(Clone, Debug)]
+pub struct GridHorizontal;
+impl Layout for GridHorizontal {
+    fn update_windows(&self, workspace: &Workspace, windows: &mut Vec<&mut &mut Window>) {
+        let window_count = windows.len() as i32;
+
+        // choose the number of columns so that we get close to an even NxN grid.
+        let num_cols = (window_count as f32).sqrt().ceil() as i32;
+
+        let mut iter = windows.iter_mut().enumerate().peekable();
+        for col in 0..num_cols {
+            let remaining_windows = window_count - iter.peek().unwrap().0 as i32;
+            let remaining_columns = num_cols - col;
+            let num_rows_in_this_col = remaining_windows / remaining_columns;
+
+            let win_height = workspace.height() / num_rows_in_this_col;
+            let win_width = workspace.width() / num_cols;
+
+            for row in 0..num_rows_in_this_col {
+                let (_idx, win) = iter.next().unwrap();
+                win.set_height(win_height);
+                win.set_width(win_width);
+                win.set_x(workspace.x() + win_width * col);
+                win.set_y(workspace.y() + win_height * row);
+            }
         }
     }
 }
