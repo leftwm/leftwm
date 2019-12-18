fcbee3c290c77b63ff36a12f9b37f1d1c1d25c25
Lex Childs <lexchilds@gmail.com>
2019-12-18 08:16:15 -0600

    Cleanup logs

--- src/models/xyhw_change.rs
+++ src/models/xyhw_change.rs
@@ -1,7 +1,6 @@
 use crate::models::Window;
 use crate::models::XYHWBuilder;
 use crate::models::XYHW;
-use log::warn;
 
 #[derive(Clone, Debug, PartialEq, Copy)]
 pub struct XYHWChange {
@@ -50,56 +49,48 @@ impl XYHWChange {
         let mut changed = false;
         if let Some(x) = self.x {
             if xyhw.x() != x {
-                warn!("CHANGE: X");
                 xyhw.set_x(x);
                 changed = true;
             }
         }
         if let Some(y) = self.y {
             if xyhw.y() != y {
-                warn!("CHANGE: Y");
                 xyhw.set_y(y);
                 changed = true;
             }
         }
         if let Some(w) = self.w {
             if xyhw.w() != w {
-                warn!("CHANGE: W");
                 xyhw.set_w(w);
                 changed = true;
             }
         }
         if let Some(h) = self.h {
             if xyhw.h() != h {
-                warn!("CHANGE: H");
                 xyhw.set_h(h);
                 changed = true;
             }
         }
         if let Some(minw) = self.minw {
             if xyhw.minw() != minw {
-                warn!("CHANGE: minw");
                 xyhw.set_minw(minw);
                 changed = true;
             }
         }
         if let Some(maxw) = self.maxw {
             if xyhw.maxw() != maxw {
-                warn!("CHANGE: maxw");
                 xyhw.set_maxw(maxw);
                 changed = true;
             }
         }
         if let Some(minh) = self.minh {
             if xyhw.minh() != minh {
-                warn!("CHANGE: minh");
                 xyhw.set_minh(minh);
                 changed = true;
             }
         }
         if let Some(maxh) = self.maxh {
             if xyhw.maxh() != maxh {
-                warn!("CHANGE: maxh");
                 xyhw.set_maxh(maxh);
                 changed = true;
             }
