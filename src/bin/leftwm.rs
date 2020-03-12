ecdfbaa1b67031f5fbd837f8e664eb5dc46c88c4
TatriX <tatrics@gmail.com>
2020-03-12 14:27:48 +0100

    refactor: Simplify leftwm main bin

--- src/bin/leftwm.rs
+++ src/bin/leftwm.rs
@@ -4,24 +4,24 @@ use std::env;
 use std::process::Command;
 
 fn main() {
-    if let Ok(booter) = std::env::current_exe() {
+    if let Ok(current_exe) = std::env::current_exe() {
         //boot everything in ~/.config/autostart
         Nanny::new().autostart();
 
         //Fix for JAVA apps so they repaint correctly
         env::set_var("_JAVA_AWT_WM_NONREPARENTING", "1");
 
-        let mut worker = booter.clone();
-        worker.pop();
-        worker.push("leftwm-worker");
+        let worker_path = current_exe.with_file_name("leftwm-worker");
 
         loop {
-            Command::new(&worker)
+            Command::new(&worker_path)
                 .status()
                 .expect("failed to start leftwm");
 
-            //left it to fast for some logging managers. We need to wait to give the logging
-            //manager a second to boot
+            // TODO: either add more details or find a better workaround.
+            //
+            // Left is to fast for some logging managers. We need to
+            // wait to give the logging manager a second to boot.
             let delay = std::time::Duration::from_millis(2000);
             std::thread::sleep(delay);
         }
