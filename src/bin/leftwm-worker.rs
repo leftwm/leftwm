bd2dfe277a756d1869bf8bdaff3a353a550830f2
TatriX <tatrics@gmail.com>
2020-03-16 11:58:51 +0100

    refactor(logs): Use only journald by default, use features for term

--- src/bin/leftwm-worker.rs
+++ src/bin/leftwm-worker.rs
@@ -112,14 +112,23 @@ fn event_loop(
 
 /// Log to both stdout and journald.
 fn setup_logging() -> slog_scope::GlobalLoggerGuard {
+    #[cfg(feature = "slog-journald")]
+    let journald = slog_journald::JournaldDrain.ignore_res();
+
+    #[cfg(feature = "slog-term")]
     let stdout = slog_term::CompactFormat::new(slog_term::TermDecorator::new().stdout().build())
         .build()
         .ignore_res();
 
-    let journald = slog_journald::JournaldDrain.ignore_res();
+    #[cfg(all(feature = "slog-journald",  feature = "slog-term"))]
+    let drain = slog::Duplicate(journald, stdout).ignore_res();
+    #[cfg(all(feature = "slog-journald",  not(feature = "slog-term")))]
+    let drain = journald;
+    #[cfg(all(not(feature = "slog-journald"),  feature = "slog-term"))]
+    let drain = stdout;
 
     // Set level filters from RUST_LOG. Defaults to `info`.
-    let envlogger = slog_envlogger::LogBuilder::new(slog::Duplicate(journald, stdout).ignore_res())
+    let envlogger = slog_envlogger::LogBuilder::new(drain)
         .parse(&std::env::var("RUST_LOG").unwrap_or_else(|_| "info".into()))
         .build()
         .ignore_res();
