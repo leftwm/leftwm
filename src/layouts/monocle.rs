d09d75fe6899b0f29d7c1e6a3d1f5d0a1748ce19
VuiMuich <jm.spam@gmx.net>
2021-02-17 20:31:25 +0100

    working monocle. Issue: .margin is not handled for monocle scope only

--- src/layouts/monocle.rs
+++ src/layouts/monocle.rs
@@ -1,15 +1,44 @@
 use crate::models::Window;
 use crate::models::Workspace;
+// use crate::models::WindowState;
 
 /// Layout which gives only one window with the full desktop realestate. A monocle mode.
 pub fn update(workspace: &Workspace, windows: &mut Vec<&mut &mut Window>) {
-    let height = workspace.height() as i32;
-    let mut y = 0;
-    for w in windows.iter_mut() {
-        w.set_height(height);
-        w.set_width(workspace.width());
-        w.set_x(workspace.x());
-        w.set_y(workspace.y() + y);
-        y += height;
+    let window_count = windows.len();
+
+    if window_count == 0 {
+        return;
+    }
+
+    let mut iter = windows.iter_mut(); 
+
+       //maximize primary window
+        
+    {
+        if let Some(monowin) = iter.next() {
+            // let mut monoclestate: Vec<WindowState> = Vec::new();
+
+            //Above, MaximizedVert, MaximizedHorz
+            // monoclestate.push(WindowState::Above);
+            // monoclestate.push(WindowState::MaximizedVert);
+            // monoclestate.push(WindowState::MaximizedHorz);
+            monowin.margin = 0;
+            monowin.set_height(workspace.height());
+            monowin.set_width(workspace.width());
+            monowin.set_x(workspace.x());
+            monowin.set_y(workspace.y());
+
+            // monowin.set_states(monoclestate.to_vec());
+            monowin.set_visible(true);
+        }
+    }
+        
+        //hide all other windows
+     {
+        if window_count > 1 {
+            for w in iter {
+                w.set_visible(false); //window state Hide
+            }
+        }
     }
 }
