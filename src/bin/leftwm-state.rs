704681b697f766aeb44625add7050507242a78a5
mautamu <mautam@usa.com>
2021-04-01 16:35:28 -0500

    Don't use /// unless doing docs

--- src/bin/leftwm-state.rs
+++ src/bin/leftwm-state.rs
@@ -120,9 +120,9 @@ fn template_handler(
 
     let mut output = template.render(&globals).unwrap();
     output = str::replace(&output, "\r", "");
-    /// We use newline rather than !newline to avoid negative logic,
-    /// but note the difference between print! and println!. Trying to skip println!
-    /// will result in theme degradation, as in #263.
+    // We use newline rather than !newline to avoid negative logic,
+    // but note the difference between print! and println!. Trying to skip println!
+    // will result in theme degradation, as in #263.
     if newline {
         print!("{}", output);
     } else {
