60de8f403c9151200b71e9428e3a3ffddd383c07
mautamu <mautam@usa.com>
2021-05-18 17:27:58 -0500

    Add fallback for git version, closes #346

--- src/bin/leftwm-check.rs
+++ src/bin/leftwm-check.rs
@@ -37,7 +37,7 @@ async fn main() -> Result<()> {
     );
     println!(
         "\x1b[0;94m::\x1b[0m LeftWM git hash: {}",
-        git_version::git_version!()
+        git_version::git_version!(fallback = "NONE")
     );
     println!("\x1b[0;94m::\x1b[0m Loading configuration . . .");
     match load_from_file(config_file, verbose) {
