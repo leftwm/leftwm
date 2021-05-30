fda2f1c16d4c22b02ad519457d6537c63d0eabc7
Michael Dorst <m@mdorst.net>
2021-05-29 20:18:32 -0700

    WIP: Add subcommands to leftwm

--- src/bin/leftwm.rs
+++ src/bin/leftwm.rs
@@ -6,7 +6,19 @@ use std::sync::{
     Arc,
 };
 
+/// Starts leftwm programs.
+///
+/// If no arguments are passed, starts `leftwm-worker`. If arguments are passed, starts
+/// `leftwm-{check, command, state, theme}` as specified, and passes along any extra arguments.
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        execute_subcommand(args);
+        return;
+    }
+
+    // If not invoked with a subcommand, start leftwm.
     if let Ok(current_exe) = std::env::current_exe() {
         //boot everything in ~/.config/autostart
         let mut children = Nanny::autostart();
@@ -41,7 +53,7 @@ fn main() {
 
             // TODO: either add more details or find a better workaround.
             //
-            // Left is to fast for some logging managers. We need to
+            // Left is too fast for some logging managers. We need to
             // wait to give the logging manager a second to boot.
             #[cfg(feature = "slow-dm-fix")]
             {
@@ -49,5 +61,31 @@ fn main() {
                 std::thread::sleep(delay);
             }
         }
+    }
+}
+
+/// Executes a subcommand.
+///
+/// If a valid subcommand is supplied, executes that subcommand, passing `args` to the program.
+/// Valid subcommands are `check`, `command`, `state` and `theme`.
+/// Prints an error to STDERR and returns `false` if an invalid subcommand is supplied.
+///
+/// # Arguments
+///
+/// + `args` - The command line arguments leftwm was called with.
+///
+/// # Panics
+///
+/// Panics if `args` has length < 2.
+fn execute_subcommand(args: Vec<String>) -> bool {
+    if ["check", "command", "state", "theme"]
+        .iter()
+        .any(|x| x == &args[1])
+    {
+        println!("Running leftwm-{} with args {:?}", &args[1], &args[2..]);
+        true
+    } else {
+        eprintln!("Invalid command '{}'.", &args[1]);
+        false
     }
 }
