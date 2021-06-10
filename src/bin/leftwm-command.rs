3587c0bd868ecbaf517383a8a6af33b99497b264
VuiMuich <jm.spam@gmx.net>
2021-06-10 21:30:17 +0200

    fmt and string formating

--- src/bin/leftwm-command.rs
+++ src/bin/leftwm-command.rs
@@ -20,7 +20,7 @@ async fn main() -> Result<()> {
             Arg::with_name("list")
                 .help("Print a list of available commands with their arguments.")
                 .short("l")
-                .long("list")
+                .long("list"),
         )
         .get_matches();
 
@@ -40,13 +40,16 @@ async fn main() -> Result<()> {
     }
 
     let command_list = matches.occurrences_of("list") == 1;
-    
+
     if command_list {
-       println!("
+        println!(
+            "
         Available Commands:
+
         Commands without arguments:
+        
         UnloadTheme
-        Reload (currently unused)
+        Reload (currently non-funct)
         ToggleFullScreen
         SwapScreens
         MoveWindowToLastWorkspace
@@ -54,7 +57,7 @@ async fn main() -> Result<()> {
         MoveWindowUp
         MoveWindowDown
         FocusWindowUp
-        MoveWindowTop (currently unused)
+        MoveWindowTop (currently non-funct)
         FocusWindowDown
         FocusNextTag
         FocusPreviousTag
@@ -65,15 +68,19 @@ async fn main() -> Result<()> {
         RotateTag
         CloseWindow
 
-        These require arguments and might be more finicky
+        Commands with arguments:
+
+        LoadTheme              Args: <Path_to/theme.toml> 
+        ToggleScratchPad       Args: <ScratchpadName>
+        SendWorkspaceToTag     Args: <workspaxe_index> <tag_index> (int)
+        SendWindowToTag        Args: <tag_index> (int)
+        SetLayout              Args: <LayoutName>
+        SetMarginMultiplier    Args: <multiplier-value> (float)
         
-        LoadTheme, Args: <Path_to/theme.toml> 
-        ToggleScratchPad, Args: <ScratchpadName>
-        SendWorkspaceToTag, Args: <workspaxe_index> <tag_index> (int)
-        SendWindowToTag, Args: <tag_index> (int)
-        SetLayout, Args: <LayoutName>
-        SetMarginMultiplier, Args: <multiplier-value> (float)
-         ") 
+        For more information please visit:
+        https://github.com/leftwm/leftwm/wiki/External-Commands
+         "
+        )
     }
     Ok(())
 }
