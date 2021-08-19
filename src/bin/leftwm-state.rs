755e2192c278aacdf290bdfffc98cf0d6f296552
Matthew Stevenson <stevensonmt@gmail.com>
2021-08-19 09:02:36 -0700

    partials test only the filename rather than filesystem IO

--- src/bin/leftwm-state.rs
+++ src/bin/leftwm-state.rs
@@ -1,6 +1,7 @@
 use clap::{value_t, App, Arg};
 use leftwm::errors::Result;
 use leftwm::models::dto::{DisplayState, ManagerState};
+use std::ffi::OsStr;
 use std::path::Path;
 use std::str;
 use tokio::fs;
@@ -70,7 +71,6 @@ async fn main() -> Result<()> {
     if let Some(template_file) = template_file {
         let path = Path::new(template_file);
         let partials = get_partials(path.parent()).await?;
-        println!("{:?}", partials);
         let template_str = fs::read_to_string(template_file).await?;
         let template = liquid::ParserBuilder::with_stdlib()
             .partials(partials)
@@ -112,15 +112,10 @@ async fn get_partials(dir: Option<&Path>) -> Result<Partials> {
     let mut partials = Partials::empty();
     match dir {
         Some(d) => {
-            let mut entries = fs::read_dir(d).await?;
-            let mut partial_paths = vec![];
-            while let Some(entry) = entries.next_entry().await? {
-                let f_n = entry.file_name();
-                let f_n_str = f_n.to_str().unwrap_or(" ");
-                if f_n_str.starts_with('_') && f_n_str.ends_with(".liquid") {
-                    partial_paths.push(entry)
-                }
-            }
+            let entries = fs::read_dir(d).await?;
+
+            let partial_paths = partials_in_dir_entries(entries).await?;
+
             for path in partial_paths {
                 partials.add(
                     path.path().as_path().to_str().unwrap_or(" "),
@@ -131,6 +126,22 @@ async fn get_partials(dir: Option<&Path>) -> Result<Partials> {
         }
         None => Ok(partials),
     }
+}
+
+async fn partials_in_dir_entries(mut entries: fs::ReadDir) -> Result<Vec<fs::DirEntry>> {
+    let mut partial_paths = vec![];
+    while let Some(entry) = entries.next_entry().await? {
+        let f_n = entry.file_name();
+        if is_partial_filename(&f_n) {
+            partial_paths.push(entry)
+        }
+    }
+    Ok(partial_paths)
+}
+
+fn is_partial_filename(filename: &OsStr) -> bool {
+    let f_n = filename.to_str().unwrap_or(" ");
+    f_n.starts_with('_') && f_n.ends_with(".liquid")
 }
 
 fn raw_handler(line: &str) -> Result<()> {
@@ -201,67 +212,22 @@ async fn stream_reader() -> Result<Lines<BufReader<UnixStream>>> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use assert_cmd::prelude::*;
-    use escargot::CargoBuild;
-    use predicates::prelude::*;
-    use std::fs::File;
-    use std::io::Write;
-    use tempfile::tempdir;
-
-    const UNKNOWN_PARTIAL_ERR: &str = r#"thread 'main' panicked at 'called `Result::unwrap()` on an `Err` value: Error { inner: InnerError { msg: Singleton("Unknown partial-template"#;
 
     #[test]
-    fn template_flag_partials() -> Result<()> {
-        // several tests included in one to avoid writing files mult times
+    fn correctly_identifies_partial_template_filenames() {
+        let file_names = vec![
+            "main.liquid",
+            "_partial.liquid",
+            "߀nonascii-in-filename.liquid", // first char U07C0
+            "1_partial.liquid",
+            "_liquid.txt",
+        ];
+        let partials = file_names
+            .iter()
+            .map(|f_n| OsStr::new(*f_n))
+            .filter(|f_n| is_partial_filename(f_n))
+            .collect::<Vec<&OsStr>>();
 
-        let temp_dir = tempdir()?;
-        let main_template_name = "temp.liquid";
-        let main_template_path = temp_dir.path().join(main_template_name);
-
-        let partial_template_name = "_partial.liquid";
-        let partial_template_path = temp_dir.path().join(partial_template_name);
-
-        // created the path but not the file yet
-        assert!(!std::path::Path::new(&partial_template_path).exists());
-
-        let mut main_template_file = File::create(&main_template_path)?;
-        let main_template_content = format!(
-            "{{% include \"{}\" %}}",
-            &partial_template_path.to_str().unwrap()
-        );
-        main_template_file.write_all(main_template_content.as_bytes())?;
-        let bin_for_test = CargoBuild::new()
-            .bin("leftwm-state")
-            .current_release()
-            .current_target()
-            .run()
-            .unwrap();
-
-        let mut cmd = bin_for_test.command();
-        cmd.arg("-t").arg(&main_template_path).arg("-q");
-
-        // run the command before creating partial file should give err:
-        cmd.assert()
-            .stderr(predicate::str::starts_with(UNKNOWN_PARTIAL_ERR))
-            .failure();
-
-        let mut partial_template_file = File::create(&partial_template_path)?;
-        partial_template_file.write_all(b"This is the partial content.")?;
-
-        // created the partial file
-        assert!(std::path::Path::new(&partial_template_path).exists());
-
-        // check that partial file gets registered and used
-        cmd.assert()
-            .stdout(predicate::str::contains(
-                partial_template_path.to_str().unwrap(),
-            ))
-            .stdout(predicate::str::contains("This is the partial content."))
-            .success();
-
-        drop(main_template_file);
-        drop(partial_template_file);
-        temp_dir.close()?;
-        Ok(())
+        assert!(partials == vec![OsStr::new("_partial.liquid")])
     }
 }
