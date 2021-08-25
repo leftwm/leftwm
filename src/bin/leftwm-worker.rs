91af2cbd19c0647191fd17566daae48090245c2e
lex148 <lexchilds@gmail.com>
2021-08-25 13:14:38 -0500

    Store hidden windows on correct sides so picom animation works (#462)

--- src/bin/leftwm-worker.rs
+++ src/bin/leftwm-worker.rs
@@ -144,7 +144,7 @@ async fn event_loop(
                 Mode::Normal => {
                     let windows: Vec<&Window> = manager.windows.iter().collect();
                     let focused = manager.focused_window();
-                    display_server.update_windows(windows, focused);
+                    display_server.update_windows(windows, focused, manager);
                     let workspaces: Vec<&Workspace> = manager.workspaces.iter().collect();
                     let focused = manager.focused_workspace();
                     display_server.update_workspaces(workspaces, focused);
@@ -156,7 +156,7 @@ async fn event_loop(
                         .iter()
                         .filter(|w| &w.handle == h)
                         .collect();
-                    display_server.update_windows(windows, focused);
+                    display_server.update_windows(windows, focused, manager);
                 }
             }
         }
