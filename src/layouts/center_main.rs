4d80c74b0e5e8290c94ff9589eb6d528353b8f82
Michael <3533648+hertg@users.noreply.github.com>
2021-09-12 02:05:43 +0200

    add missing comma to satisfy fmt

--- src/layouts/center_main.rs
+++ src/layouts/center_main.rs
@@ -66,7 +66,7 @@ pub fn update(workspace: &Workspace, windows: &mut Vec<&mut Window>, tags: &mut 
 
     let primary_width = match window_count {
         1 => workspace.width() as i32,
-        _ => ((workspace.width() as f32 / 100.0) * workspace.main_width(tags)).floor() as i32
+        _ => ((workspace.width() as f32 / 100.0) * workspace.main_width(tags)).floor() as i32,
     };
 
     let secondary_width = match window_count {
