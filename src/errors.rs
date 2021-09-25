26f2538b75c64ae0cceaaf1b4000258fad3cad14
Cecile Tonglet <cecile.tonglet@cecton.com>
2021-09-25 15:01:39 +0100

    More todos

--- src/errors.rs
+++ src/errors.rs
@@ -4,10 +4,12 @@ pub type Result<T> = std::result::Result<T, LeftError>;
 
 #[derive(Debug, Error)]
 pub enum LeftError {
+    // TODO move StateSocket away from lib OR use Config::save_state?
     #[error("Parsing error: {0}")]
     SerdeParse(#[from] serde_json::error::Error),
     #[error("IO error: {0}")]
     IoError(#[from] std::io::Error),
+    // TODO move Nanny to bin
     #[error("XDG error: {0}")]
     XdgBaseDirError(#[from] xdg::BaseDirectoriesError),
     #[error("Stream error")]
