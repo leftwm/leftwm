ef4e8065d84f8e3c0a0519bde7d091d12da50bea
Cecile Tonglet <cecile.tonglet@cecton.com>
2021-10-02 20:57:43 +0100

    Make 0 or more than 4 fail

--- src/bin/common/theme_setting.rs
+++ src/bin/common/theme_setting.rs
@@ -64,32 +64,19 @@ impl std::convert::TryFrom<CustomMargins> for Margins {
     fn try_from(c: CustomMargins) -> Result<Self, Self::Error> {
         match c {
             CustomMargins::Int(size) => Ok(Self::new(size)),
-            CustomMargins::Vec(vec) => {
-                match vec.len() {
-                    1 => Ok(Self::new(vec[0])),
-                    2 => Ok(Self::new_from_pair(vec[0], vec[1])),
-                    3 => Ok(Self::new_from_triple(vec[0], vec[1], vec[2])),
-                    4 => Ok(Self {
-                        top: vec[0],
-                        right: vec[1],
-                        bottom: vec[2],
-                        left: vec[3],
-                    }),
-                    0 => {
-                        log::error!("Empty margin or border array");
-                        Ok(Self::new(10)) //assume 10 px borders for now
-                    }
-                    _ => {
-                        log::error!("Too many entries in margin or border array");
-                        Ok(Self {
-                            top: vec[0],
-                            right: vec[1],
-                            bottom: vec[2],
-                            left: vec[3],
-                        })
-                    }
-                }
-            }
+            CustomMargins::Vec(vec) => match vec.len() {
+                1 => Ok(Self::new(vec[0])),
+                2 => Ok(Self::new_from_pair(vec[0], vec[1])),
+                3 => Ok(Self::new_from_triple(vec[0], vec[1], vec[2])),
+                4 => Ok(Self {
+                    top: vec[0],
+                    right: vec[1],
+                    bottom: vec[2],
+                    left: vec[3],
+                }),
+                0 => Err("Empty margin or border array"),
+                _ => Err("Too many entries in margin or border array"),
+            },
         }
     }
 }
