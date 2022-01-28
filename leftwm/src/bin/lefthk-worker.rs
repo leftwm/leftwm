452ec3b5e79efb26e04e9f10af5686a79597a05a
AethanFoot <aethanfoot@hotmail.com>
2022-01-28 15:26:37 +0000

    Initial

---
+++ leftwm/src/bin/lefthk-worker.rs
@@ -0,0 +1,58 @@
+use lefthk_core::worker::Worker;
+use slog::{o, Drain};
+use xdg::BaseDirectories;
+
+fn main() {
+    let _log_guard = setup_logging();
+    let config = leftwm::load();
+    let path =
+        BaseDirectories::with_prefix("leftwm").expect("ERROR: could not find base directory");
+    let config_file = path
+        .place_config_file("config.toml")
+        .expect("ERROR: could not find config file");
+    let completed = std::panic::catch_unwind(|| {
+        let rt = tokio::runtime::Runtime::new().expect("ERROR: couldn't init Tokio runtime");
+        let _rt_guard = rt.enter();
+        let mut worker = Worker::new(config.mapped_bindings(), config_file.clone());
+
+        rt.block_on(worker.event_loop());
+    });
+
+    match completed {
+        Ok(_) => log::info!("Completed"),
+        Err(err) => log::error!("Completed with error: {:?}", err),
+    }
+}
+
+/// Log to both stdout and journald.
+#[allow(dead_code)]
+fn setup_logging() -> slog_scope::GlobalLoggerGuard {
+    #[cfg(feature = "slog-journald")]
+    let journald = slog_journald::JournaldDrain.ignore_res();
+
+    #[cfg(feature = "slog-term")]
+    let stdout = slog_term::CompactFormat::new(slog_term::TermDecorator::new().stdout().build())
+        .build()
+        .ignore_res();
+
+    #[cfg(all(feature = "slog-journald", feature = "slog-term"))]
+    let drain = slog::Duplicate(journald, stdout).ignore_res();
+    #[cfg(all(feature = "slog-journald", not(feature = "slog-term")))]
+    let drain = journald;
+    #[cfg(all(not(feature = "slog-journald"), feature = "slog-term"))]
+    let drain = stdout;
+
+    // Set level filters from RUST_LOG. Defaults to `info`.
+    let envlogger = slog_envlogger::LogBuilder::new(drain)
+        .parse(&std::env::var("RUST_LOG").unwrap_or_else(|_| "info".into()))
+        .build()
+        .ignore_res();
+
+    let logger = slog::Logger::root(slog_async::Async::default(envlogger).ignore_res(), o!());
+
+    slog_stdlog::init().unwrap_or_else(|err| {
+        eprintln!("failed to setup logging: {}", err);
+    });
+
+    slog_scope::set_global_logger(logger)
+}
