e5633235e387939a97fc752967e32b4877dafb64
TornaxO7 <tornax07@gmail.com>
2022-09-28 18:08:51 +0200

    applying Aethen's suggestion

--- leftwm-core/src/handlers/window_handler.rs
+++ leftwm-core/src/handlers/window_handler.rs
@@ -311,16 +311,12 @@ fn set_relative_floating(window: &mut Window, ws: &Workspace, outer: Xyhw) {
     let xyhw = window.requested.map_or_else(
         || ws.center_halfed(),
         |mut requested| {
+            requested.center_relative(outer, window.border);
             if ws.xyhw.contains_xyhw(&requested) {
-                requested.center_relative(ws.xyhw, window.border);
                 requested
             } else {
-                requested.center_relative(outer, window.border);
-                if ws.xyhw.contains_xyhw(&requested) {
-                    requested
-                } else {
-                    ws.center_halfed()
-                }
+                requested.center_relative(ws.xyhw, window.border);
+                requested
             }
         },
     );
