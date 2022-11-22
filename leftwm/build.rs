fd026793ee544f8e1fd5301a2c70144cc1d95d60
pin <90570748+0323pin@users.noreply.github.com>
2022-11-22 11:57:16 +0100

    Exclude NetBSD in `build.rs` and avoid build-hangs (#962)

--- leftwm/build.rs
+++ leftwm/build.rs
@@ -13,7 +13,7 @@ fn main() {
 
     println!("cargo:rustc-env=LEFTWM_FEATURES={}", features_string);
 
-    #[cfg(feature = "lefthk")]
+    #[cfg(all(feature = "lefthk", not(target_os = "netbsd")))]
     match std::process::Command::new("lefthk-worker").spawn() {
         Ok(mut p) => p.kill().unwrap(),
         Err(_) => println!("cargo:warning=When first time building with `lefthk` you need to completely restart `leftwm` in order to start the hotkey daemon proprerly. A `SoftReload` or `HardReload` will leave you with a session non responsive to keybinds but otherwise running well."),
