1f031138b264e9583e4c975024ee09c6239114ac
hertg <3533648+hertg@users.noreply.github.com>
2023-03-10 23:50:33 +0100

    fix capitalization for logging outputs

--- leftwm-core/src/handlers/screen_create_handler.rs
+++ leftwm-core/src/handlers/screen_create_handler.rs
@@ -7,7 +7,7 @@ impl<C: Config, SERVER: DisplayServer> Manager<C, SERVER> {
     ///
     /// Returns `true` if changes need to be rendered.
     pub fn screen_create_handler(&mut self, screen: Screen) -> bool {
-        tracing::trace!("screen create: {:?}", screen);
+        tracing::trace!("Screen create: {:?}", screen);
 
         let tag_index = self.state.workspaces.len();
         let tag_len = self.state.tags.len_normal();
