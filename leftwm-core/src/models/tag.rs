leftwm/leftwm

commit 4621dab374084fac6022a5e148a4aa8620ce15ec

Misc optimizations / corrections (#1159)

leftwm-core/src/models/tag.rs
@@ -231,6 +231,7 @@ impl Tag {
         {
             window.set_visible(true);
             window.normal = workspace.xyhw;
+
             let handle = window.handle;
             windows
                 .iter_mut()
@@ -246,33 +247,14 @@ impl Tag {
             .iter_mut()
             .find(|w| w.has_tag(&self.id) && w.is_maximized())
         {
-            // Update maximized window
             window.set_visible(true);
-            window.normal = Xyhw::from(workspace.rect());
-            let handle = window.handle;
-            windows
-                .iter_mut()
-                .filter(|w| {
-                    w.has_tag(&self.id)
-                        && w.transient.unwrap_or_else(|| 0.into()) == handle
-                        && w.is_managed()
-                })
-                .for_each(|w| {
-                    w.set_visible(true);
-                });
+            window.normal = workspace.rect().into();
 
-            // Update all windows except normal non-floating windoows and maximized window
             windows
                 .iter_mut()
-                .filter(|w| {
-                    w.has_tag(&self.id) && (!w.is_normal() || w.floating()) && !w.is_maximized()
-                })
+                .filter(|w| w.has_tag(&self.id) && w.floating() && w.is_managed())
                 .for_each(|w| {
                     w.set_visible(true);
-                    // Don't change docks and desktop xyhw
-                    if w.is_managed() {
-                        w.normal = workspace.xyhw;
-                    }
                 });
         } else {
             // Don't bother updating the other windows when a window is fullscreen.

leftwm-core/src/handlers/window_handler.rs
@@ -19,7 +19,7 @@ impl<C: Config, SERVER: DisplayServer> Manager<C, SERVER> {
             return false;
         }
 
-        // Setup any predifined hooks.
+        // Setup any predefined hooks.
         self.config
             .setup_predefined_window(&mut self.state, &mut window);
         let mut is_first = false;

leftwm-core/src/state.rs
@@ -171,7 +171,6 @@ impl State {
     }
 
     pub fn update_static(&mut self) {
-        let workspaces = self.workspaces.clone();
         self.windows
             .iter_mut()
             .filter(|w| w.strut.is_some() || w.is_sticky())
@@ -180,7 +179,7 @@ impl State {
                     Some(strut) => strut.center(),
                     None => w.calculated_xyhw().center(),
                 };
-                if let Some(ws) = workspaces.iter().find(|ws| ws.contains_point(x, y)) {
+                if let Some(ws) = self.workspaces.iter().find(|ws| ws.contains_point(x, y)) {
                     w.tag = ws.tag;
                 }
             });

commit 73149fef25c788f2443e6f09e0ff4f44ade4de06

Fix regression introduced in 4621dab (#1164)

leftwm-core/src/models/tag.rs
@@ -252,7 +252,7 @@ impl Tag {
 
             windows
                 .iter_mut()
-                .filter(|w| w.has_tag(&self.id) && w.floating() && w.is_managed())
+                .filter(|w| w.has_tag(&self.id) && w.floating())
                 .for_each(|w| {
                     w.set_visible(true);
                 });