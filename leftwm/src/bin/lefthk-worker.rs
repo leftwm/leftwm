7c65998291fc5dcffecf9ba43dea08a6d57116fb
Eskaan <github@eskaan.de>
2023-12-02 22:26:35 +0100

    lefthk-worker: change multiple feature checks to one module condition

--- leftwm/src/bin/lefthk-worker.rs
+++ leftwm/src/bin/lefthk-worker.rs
@@ -1,28 +1,25 @@
-#[cfg(feature = "lefthk")]
+#![cfg(feature = "lefthk")]
+
 use lefthk_core::{config::Config, worker::Worker};
-#[cfg(feature = "lefthk")]
 use xdg::BaseDirectories;
 
 fn main() {
-    #[cfg(feature = "lefthk")]
-    {
-        leftwm::utils::log::setup_logging();
+    leftwm::utils::log::setup_logging();
 
-        tracing::info!("lefthk-worker booted!");
+    tracing::info!("lefthk-worker booted!");
 
-        let exit_status = std::panic::catch_unwind(|| {
-            let rt = tokio::runtime::Runtime::new().expect("ERROR: couldn't init Tokio runtime");
-            let _rt_guard = rt.enter();
-            let config = leftwm::load();
-            let path = BaseDirectories::with_prefix("leftwm-lefthk")
-                .expect("ERROR: could not find base directory");
+    let exit_status = std::panic::catch_unwind(|| {
+        let rt = tokio::runtime::Runtime::new().expect("ERROR: couldn't init Tokio runtime");
+        let _rt_guard = rt.enter();
+        let config = leftwm::load();
+        let path = BaseDirectories::with_prefix("leftwm-lefthk")
+            .expect("ERROR: could not find base directory");
 
-            rt.block_on(Worker::new(config.mapped_bindings(), path).event_loop());
-        });
+        rt.block_on(Worker::new(config.mapped_bindings(), path).event_loop());
+    });
 
-        match exit_status {
-            Ok(()) => tracing::info!("Completed"),
-            Err(err) => tracing::error!("Completed with error: {:?}", err),
-        }
+    match exit_status {
+        Ok(()) => tracing::info!("Completed"),
+        Err(err) => tracing::error!("Completed with error: {:?}", err),
     }
 }
