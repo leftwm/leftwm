0951a85d67b8e8539e8085bf71058f08a1549eae
Eskaan <github@eskaan.de>
2023-12-18 18:59:58 +0100

    Move clippy allowance to xlib-display-server, deprecate `make test-full`

--- display-servers/xlib-display-server/src/lib.rs
+++ display-servers/xlib-display-server/src/lib.rs
@@ -1,3 +1,11 @@
+// allow casting types
+#![allow(clippy::must_use_candidate)]
+#![allow(clippy::cast_precision_loss)]
+#![allow(clippy::cast_possible_truncation)]
+#![allow(clippy::cast_possible_wrap)]
+#![allow(clippy::cast_sign_loss)]
+#![allow(clippy::mut_mut)]
+
 mod event_translate;
 mod event_translate_client_message;
 mod event_translate_property_notify;
